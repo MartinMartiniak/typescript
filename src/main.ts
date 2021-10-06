e77e7359b3d09655a2e6a72a9967eed92840aa17
Martin Martiniak <martiniak.martin15@gmail.com>
2021-10-06 21:18:51 +0200

    .

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,9 @@
-const message: string = 'Hello AppsLab 2020!';
-console.log(message);
+const heightOfStep = 0.3;
+const lenghtOfStep = 0.2;
+const heightOfTower = 25;
+
+console.log(totalDistance(heightOfStep, lenghtOfStep, heightOfTower))
+
+
+function totalDistance(heightOfStep, lenghtOfStep, heightOfTower){
+            return(heightOfTower/heightOfStep)*(heightOfStep+lenghtOfStep);}
