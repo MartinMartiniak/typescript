122c3ddc90992a33a0666ccb3d062aa3c7651eee
Martin Martiniak <martiniak.martin15@gmail.com>
2021-10-06 21:19:32 +0200

    .

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
-const heightOfStep = 0.3;
-const lenghtOfStep = 0.2;
-const heightOfTower = 25;
+const number1 = 3;
+const number2 = 4;
+const number3 = 3;
 
-console.log(totalDistance(heightOfStep, lenghtOfStep, heightOfTower))
+console.log(EqualityOfThreeValues(number1, number2, number3))
 
-
-function totalDistance(heightOfStep, lenghtOfStep, heightOfTower){
-            return(heightOfTower/heightOfStep)*(heightOfStep+lenghtOfStep);}
+function EqualityOfThreeValues(number1, number2, number3){
+            if(number1 == number2 && number2 == number3){
+                        return 3;
+            }
+            else if(number1 == number2 || number1 == number3 || number2 == number3){
+                        return 2;
+            }
+            else{
+                        return 0;
+            }
+}
